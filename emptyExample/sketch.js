40f77e625b7fef208c80d891790c89e16d5216d3
Adele Gikonyo <adele.gikonyo@gmail.com>
2021-04-27 16:17:58 +0300

    Made code tidier with the use to vectors to describe location and movement

--- emptyExample/sketch.js
+++ emptyExample/sketch.js
@@ -1,8 +1,43 @@
+var ball;
+
 function setup() {
     createCanvas(900, 600);
-    background(0);
+    ball = new Ball;
+    
 }
 
 function draw() {
+    background(0);
+    ball.run();
+    
 
+}
+
+
+class Ball {
+    constructor(){
+        this.velocity = new createVector(random(-2,2), random(-2,2));
+        this.location = new createVector(random(width), random(height));
+        
+    }
+    
+    run(){
+        this.draw();
+        this.move();
+        this.bounce();
+    }
+    
+    draw(){
+        fill(125);
+        ellipse(this.location.x, this.location.y, 40, 40);
+    }
+    
+    move(){
+        this.location.add(this.velocity);
+    }
+    
+    bounce(){
+        if (this.location.x < 0 || this.location.x > width) this.velocity.x *= -1;
+        if (this.location.y < 0 || this.location.y > height) this.velocity.y *= -1;
+    }
 }
