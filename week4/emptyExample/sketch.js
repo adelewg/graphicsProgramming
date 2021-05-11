b3b82b35cbe2bdf6a22981a8900160df4d8d301e
Adele Gikonyo <adele.gikonyo@gmail.com>
2021-05-11 11:57:19 +0300

    started working with the matter.js library physics engine to create a simple example of a box falling down and hitting the ground

--- week4/emptyExample/sketch.js
+++ week4/emptyExample/sketch.js
@@ -1,7 +1,51 @@
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+    World = Matter.World;
+
+var engine;
+var box1;
+var ground;
+
+
 function setup() {
+    createCanvas(900, 600);
+    
+    //create an engine
+    engine = Engine.create();
+    
+    box1 = Bodies.rectangle(200, 200, 80, 80, {restitution: .8, friction: 0.01});
+    
+    var options = {isStatic: true, angle: Math.PI * 0.06};
+    ground = Bodies.rectangle(400, 500, 810, 10, options);
+    
+    //add all of the bodies to the world
+    World.add(engine.world, [box1, ground]);
 
 }
 
 function draw() {
+    background(0);
+    Engine.update(engine);
+    
+    fill(255);
+    drawVertices(box1.vertices);
+    
+    fill(125);
+    drawVertices(ground.vertices);
+  
 
+}
+
+function drawVertices(vertices)
+{
+    beginShape();
+    for(var i = 0; i < vertices.length; i++)
+        {
+            vertex(vertices[i].x, vertices[i].y);
+        }
+    endShape(CLOSE);
 }
