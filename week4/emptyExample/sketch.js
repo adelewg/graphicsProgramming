646b0a77f33e3e6d9af7956d1d57fea52d06c3f2
Adele Gikonyo <adele.gikonyo@gmail.com>
2021-05-12 15:57:52 +0300

    added an array of boxes to the physics world

--- week4/emptyExample/sketch.js
+++ week4/emptyExample/sketch.js
@@ -8,7 +8,9 @@ var Engine = Matter.Engine,
 
 var engine;
 var box1;
-var ground;
+var ground1, ground2;
+var circle, polygon;
+var boxes = [];
 
 
 function setup() {
@@ -19,11 +21,13 @@ function setup() {
     
     box1 = Bodies.rectangle(200, 200, 80, 80, {restitution: .8, friction: 0.01});
     
-    var options = {isStatic: true, angle: Math.PI * 0.06};
-    ground = Bodies.rectangle(400, 500, 810, 10, options);
+    
+   
+    ground1 = Bodies.rectangle(100, 200, 500, 10, {isStatic: true, angle: Math.PI * 0.06});
+    ground2 = Bodies.rectangle(400, 500, 500, 10, {isStatic: true, angle: Math.PI * -0.06});
     
     //add all of the bodies to the world
-    World.add(engine.world, [box1, ground]);
+    World.add(engine.world, [box1, ground1, ground2]);
 
 }
 
@@ -34,10 +38,25 @@ function draw() {
     fill(255);
     drawVertices(box1.vertices);
     
+    generateObjects(width/2, 0);
+    
+    for(var i = 0; i < boxes.length; i++)
+        {
+            drawVertices(boxes[i].vertices);
+        }
+   
+    
     fill(125);
-    drawVertices(ground.vertices);
+    drawVertices(ground1.vertices);
+    drawVertices(ground2.vertices);
   
 
+}
+
+function generateObjects(x, y) {
+    var b = Bodies.rectangle(x, y, random(10,30), random(10,30), {restitution: 0.8, friction: 0.5});
+    boxes.push(b);
+    World.add(engine.world, [b]);
 }
 
 function drawVertices(vertices)
